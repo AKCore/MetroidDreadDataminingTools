4abf061bb545ae20d97300432efaeafb5fe45844
AKCore <dblegendsmud@gmail.com>
2021-10-09 07:41:07 -0700

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace MetroidDreadDatamine
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new DarkUI.Controls.DarkButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.className = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ClassType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Damage = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Checked = false;
+            this.btnLoad.Location = new System.Drawing.Point(12, 55);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(110, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load Tunables";
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.className,
+            this.ClassType,
+            this.Damage});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 85);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(775, 353);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // className
+            // 
+            this.className.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.className.Frozen = true;
+            this.className.HeaderText = "Class Name";
+            this.className.Name = "className";
+            this.className.Width = 88;
+            // 
+            // ClassType
+            // 
+            this.ClassType.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.ClassType.Frozen = true;
+            this.ClassType.HeaderText = "Class Type";
+            this.ClassType.Name = "ClassType";
+            this.ClassType.Width = 84;
+            // 
+            // Damage
+            // 
+            this.Damage.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Damage.Frozen = true;
+            this.Damage.HeaderText = "Damage";
+            this.Damage.Name = "Damage";
+            this.Damage.Width = 72;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DarkUI.Controls.DarkButton btnLoad;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn className;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ClassType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Damage;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarkUI.Forms;
+using DarkUI.Collections;
+using DarkUI.Controls;
+namespace MetroidDreadDatamine
+{
+    public partial class MainForm : DarkForm
+    {
+        public byte[] tunablesFile;
+        public struct DamagableData
+        {
+            public string ClassName;
+            public short ClassType;
+            public float Damage;
+        }
+
+        public struct DamagableLocation
+        {
+            public DamagableLocation(int x, int y, int z)
+            {
+                offset = x;
+                textLen = y;
+                damageOffset = z;
+            }
+
+            public int offset;
+            public int textLen;
+            public int damageOffset;
+        }
+
+        public List<DamagableData> damagables = new List<DamagableData>();
+        public List<DamagableLocation> damageLocs = new List<DamagableLocation>();
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tunablesFile = File.ReadAllBytes("tunables.btunda");
+
+                SearchForTunable();
+                foreach (DamagableLocation d in damageLocs)
+                {
+                    ConvertToDamagablesData(d.offset, d.textLen, d.damageOffset);
+                }
+
+                foreach (DamagableData d in damagables)
+                {
+                    Console.WriteLine("ClassName:" + d.ClassName);
+                    Console.WriteLine("ClassType:" + d.ClassType);
+                    Console.WriteLine("Damage:" + d.Damage);
+                    DataGridViewRow row = (DataGridViewRow)dataGridView1.RowTemplate.Clone();
+                    row.CreateCells(dataGridView1, d.ClassName, d.ClassType, d.Damage);
+                    dataGridView1.Rows.Add(row);
+                }
+
+
+            }
+
+            catch (Exception ex)
+            {
+                DarkMessageBox.Show(this, ex.StackTrace, "Failed To Load Data", MessageBoxButtons.OK);
+            }
+        }
+
+        private void ConvertToDamagablesData(int offset, int textLen, int damageOffset)
+        {
+            try
+            {
+                DamagableData data = new DamagableData();
+                List<byte> bytes = new List<byte>();
+
+                for (int i = 0; i < textLen; i++)
+                    bytes.Add(tunablesFile[offset + i]);
+
+                byte[] textbytes = bytes.ToArray();
+
+                data.ClassName = Encoding.ASCII.GetString(textbytes);
+
+                data.ClassType = BitConverter.ToInt16(new byte[] { tunablesFile[offset + textLen + 1], tunablesFile[offset + textLen + 2] }, 0);
+
+                byte[] damageFloat = new byte[] { tunablesFile[offset + textLen + damageOffset + 1], tunablesFile[offset + textLen + damageOffset + 2], tunablesFile[offset + textLen + damageOffset + 3], tunablesFile[offset + textLen + damageOffset + 4] };
+                data.Damage = ToFloat(damageFloat);
+
+                damagables.Add(data);
+            }
+            catch(Exception ex)
+            {
+                //DarkMessageBox.Show(this, ex.StackTrace, "Failed To Load Data", MessageBoxButtons.OK);
+            }
+        }
+
+        static float ToFloat(byte[] input)
+        {
+            try
+            {
+                byte[] newArray = new[] { input[0], input[1], input[2], input[3] };
+                return BitConverter.ToSingle(newArray, 0);
+            }
+            catch { return 0; }
+        }
+        public byte[] tunableString = new byte[] {0x43, 0x54, 0x75, 0x6e, 0x61, 0x62, 0x6c, 0x65 };
+        public void SearchForTunable()
+        {
+            int lastFind = 0;
+            
+            for (int tunable = 0; tunable < tunablesFile.Length;)
+            {
+                try
+                {
+                    DamagableLocation loc = new DamagableLocation();
+                    int strLen = 0;
+                    List<byte> strList = new List<byte>();
+                        
+                    lastFind = ByteSearch(tunablesFile, tunableString, tunable);
+                    if (lastFind != -1)
+                    {
+                        strList.Clear();
+                        bool reading = true;
+                        int j = 0;
+                        while (reading)
+                        {
+                            if (tunablesFile[j + lastFind] != 0)
+                            {
+                                strList.Add(tunablesFile[j + lastFind]);
+                            }
+                            else
+                            {
+                                reading = false;
+                                strLen = strList.Count;
+                                loc.textLen = strLen;
+                            }
+                            j++;
+                        }
+                        int type = BitConverter.ToInt16(tunablesFile, lastFind + strLen + 1);
+                        if (type == 17001) // This type is Samus' weapon. 
+                        {
+                            //Found a damagable struct
+                            loc.offset = lastFind;
+                            Console.WriteLine("Found type at " + lastFind);
+                            loc.damageOffset = 8;
+                            Console.WriteLine("Found damage at " + (lastFind + strLen + 1 + 2 + 5));
+                            damageLocs.Add(loc);
+                            tunable = (strLen + lastFind);
+
+                        }
+                        else
+                        {
+                            tunable = (lastFind + strLen);
+                        }
+                    }
+                    else
+                        return;
+                }
+                catch(Exception ex)
+                {
+                    tunable += 1;
+                    Console.WriteLine(ex.StackTrace);
+                    break;
+                }
+            }
+        }
+        static int ByteSearch(byte[] searchIn, byte[] searchBytes, int start = 0)
+        {
+            int found = -1;
+            bool matched = false;
+            //only look at this if we have a populated search array and search bytes with a sensible start
+            if (searchIn.Length > 0 && searchBytes.Length > 0 && start <= (searchIn.Length - searchBytes.Length) && searchIn.Length >= searchBytes.Length)
+            {
+                //iterate through the array to be searched
+                for (int i = start; i <= searchIn.Length - searchBytes.Length; i++)
+                {
+                    //if the start bytes match we will start comparing all other bytes
+                    if (searchIn[i] == searchBytes[0])
+                    {
+                        if (searchIn.Length > 1)
+                        {
+                            //multiple bytes to be searched we have to compare byte by byte
+                            matched = true;
+                            for (int y = 1; y <= searchBytes.Length - 1; y++)
+                            {
+                                if (searchIn[i + y] != searchBytes[y])
+                                {
+                                    matched = false;
+                                    break;
+                                }
+                            }
+                            //everything matched up
+                            if (matched)
+                            {
+                                found = i;
+                                break;
+                            }
+
+                        }
+                        else
+                        {
+                            //search byte is only one bit nothing else to do
+                            found = i;
+                            break; //stop the loop
+                        }
+
+                    }
+                }
+
+            }
+            return found;
+        }
+    }
+}
